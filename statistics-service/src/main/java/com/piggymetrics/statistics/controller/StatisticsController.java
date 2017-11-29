61dd3f8f7b82472faf11de2b425368b3947f94e1
Haitao Xing <htxing@alauda.io>
2017-11-29 19:31:05 +0800

    Added logic to simulate timeout and execption cases for verifying circuit breaker.

--- statistics-service/src/main/java/com/piggymetrics/statistics/controller/StatisticsController.java
+++ statistics-service/src/main/java/com/piggymetrics/statistics/controller/StatisticsController.java
@@ -3,6 +3,9 @@ package com.piggymetrics.statistics.controller;
 import com.piggymetrics.statistics.domain.Account;
 import com.piggymetrics.statistics.domain.timeseries.DataPoint;
 import com.piggymetrics.statistics.service.StatisticsService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,8 @@ import java.util.List;
 @RestController
 public class StatisticsController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private StatisticsService statisticsService;
 
@@ -31,6 +36,21 @@ public class StatisticsController {
 	@PreAuthorize("#oauth2.hasScope('server')")
 	@RequestMapping(value = "/{accountName}", method = RequestMethod.PUT)
 	public void saveAccountStatistics(@PathVariable String accountName, @Valid @RequestBody Account account) {
+		
+		// Use user timeout to simulate timeout 
+		if ("timeout".equalsIgnoreCase(accountName)) {
+			try {
+				Thread.sleep(3 * 1000);
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
+		
+		// Use user timeout to simulate exception 
+		if ("exception".equalsIgnoreCase(accountName)) {
+			throw new RuntimeException("fake statistics exception");
+		}
+		
 		statisticsService.save(accountName, account);
 	}
 }
