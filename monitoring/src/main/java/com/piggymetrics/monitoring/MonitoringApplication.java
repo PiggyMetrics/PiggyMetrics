f45220a61d468607fe05da5dd7bd560548260595
PiggyMetrics <htxing@alauda.io>
2018-06-26 22:35:59 +0800

    Added controller for monitering. User in-memory storage for zipkip.

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -5,14 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableTurbineStream
 @EnableHystrixDashboard
 @EnableDiscoveryClient
+@Controller
 public class MonitoringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitoringApplication.class, args);
 	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+
 }
